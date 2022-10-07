d3467608f4a97b7e3d1820dc06939763bfb29345
Caio Pereira Antunes <caioefg@gmail.com>
2022-10-06 21:57:49 -0300

    verificado github

--- src/models/connection.ts
+++ src/models/connection.ts
@@ -6,8 +6,7 @@ dotenv.config();
 const connection = mysql.createPool({
   host: process.env.MYSQL_HOST || 'localhost',
   user: process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQL_PASSWORD || '123456',
-  database: process.env.MYSQL_DATABASE,
+  password: process.env.MYSQL_PASSWORD || 'password',
 });
 
 export default connection;
